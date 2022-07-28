58448d2553526f6aa67f17580a77cfd65b4941f4
Joe Chan <joech4n@users.noreply.github.com>
2022-07-27 17:34:21 -0700

    Remove install of jsc.

--- javascript/install-Darwin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if test ! $(which jsc)
-then
-  echo "  Linking OSX's jsc for you."
-  sudo ln -s -f /System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Resources/jsc /usr/local/bin/jsc
-fi
